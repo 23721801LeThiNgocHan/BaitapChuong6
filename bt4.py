301d4aa20885c48c094e8a0a6c9b8f198511a15e
23721801LeThiNgocHan <letnhan911920td@gmail.com>
2024-08-25 16:44:22 +0700

    Add files via upload

--- bt4.py
+++ bt4.py
@@ -10,3 +10,5 @@ hh, mm, ss = map(int, time.split(":"))
 if 0<= hh <24 and 0<=mm<60 and 0<=ss<60:
     T= hh*3600 + mm*60 + ss
     print("Đổi được là: ",T)
+else:
+    print("Không định dạng được")
